e74decd5c76d924a9ab87a5c46152b0e738d6902
poypoy252525 <61503297+poypoy252525@users.noreply.github.com>
2023-05-03 22:41:10 +0800

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.input = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.output = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'input
+        '
+        Me.input.Location = New System.Drawing.Point(178, 113)
+        Me.input.Name = "input"
+        Me.input.Size = New System.Drawing.Size(100, 20)
+        Me.input.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(133, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'output
+        '
+        Me.output.Location = New System.Drawing.Point(178, 164)
+        Me.output.Name = "output"
+        Me.output.Size = New System.Drawing.Size(100, 20)
+        Me.output.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 349)
+        Me.Controls.Add(Me.output)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.input)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents input As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents output As TextBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles input.TextChanged
+        If input.Text = "a" Or input.Text = "A" Then
+            output.Text = "It's a vowel!"
+        ElseIf input.Text = "e" Or input.Text = "E" Then
+            output.Text = "It's a vowel!"
+        ElseIf input.Text = "i" Or input.Text = "I" Then
+            output.Text = "It's a vowel!"
+        ElseIf input.Text = "o" Or input.Text = "O" Then
+            output.Text = "It's a vowel!"
+        ElseIf input.Text = "u" Or input.Text = "U" Then
+            output.Text = "It's a vowel!"
+        Else
+            output.Text = "It's a consonant!"
+        End If
+
+    End Sub
+End Class
